795ac6b5c10e39ad96a161fc1c72cb701b94a9e3
Jon <jon@jon.systems>
2025-03-06 04:49:05 +0000

    Frontend: Add ShadCN UI library & required components

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,61 +1,6 @@
-import { and, eq, gt, lt } from "drizzle-orm";
-import { db } from "~/server/db";
-import { type repeatingSlotRules, slots } from "~/server/db/schema";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-export const difMins = (a: Date, b: Date) =>
-  Math.round((a.getTime() - b.getTime()) / 60000);
-
-export const minsToMs = (mins: number) => mins * 60000;
-
-export type RuleGeneratedSlot = {
-  startsAt: Date;
-  endsAt: Date;
-  duration: string;
-  repeatingRuleId: string;
-};
-
-export const generateSlotsFromRepeatingRule = async (
-  repeatingRule: typeof repeatingSlotRules.$inferSelect,
-) => {
-  const durationMins = repeatingRule.repeatingDuration === "15m" ? 15 : 30;
-  const startTime = new Date(repeatingRule.startTime);
-  const endTime = new Date(repeatingRule.endTime);
-
-  const minsBetween = difMins(endTime, startTime);
-  const totalSlots = Math.round(minsBetween / durationMins);
-
-  const slotObjects = Array.from({
-    length: totalSlots,
-  }).map((_, index) => {
-    const startsAt = new Date(
-      startTime.getTime() + minsToMs(index * durationMins),
-    );
-    const endsAt = new Date(startsAt.getTime() + minsToMs(durationMins));
-
-    return {
-      startsAt,
-      endsAt,
-      duration: repeatingRule.repeatingDuration,
-      repeatingRuleId: repeatingRule.id,
-    };
-  });
-
-  const ensuredSlots = await Promise.all(
-    slotObjects.map(async (slot) => {
-      const slotConflict = await db.query.slots.findFirst({
-        where: and(
-          eq(slots.doctorId, repeatingRule.doctorId),
-          lt(slots.startsAt, slot.endsAt),
-          gt(slots.endsAt, slot.startsAt),
-        ),
-      });
-      if (slotConflict) return false;
-      return slot;
-    }),
-  );
-  const generatedSlots: RuleGeneratedSlot[] = ensuredSlots.filter(
-    (s) => s !== false,
-  );
-
-  return generatedSlots;
-};
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
