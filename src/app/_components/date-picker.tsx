4db778d21cad6d68d8b2060a30bf568d50a45b57
Jon <jon@jon.systems>
2025-03-06 21:06:04 +0000

    Frontend: Add datepicker and underyling ShadCN components

---
+++ src/app/_components/date-picker.tsx
@@ -0,0 +1,45 @@
+import { format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { Button } from "~/components/ui/button";
+import { Calendar } from "~/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "~/components/ui/popover";
+import { cn } from "~/lib/utils";
+
+const DatePicker = ({
+  value,
+  onValueChange,
+}: {
+  value: undefined | Date;
+  onValueChange: (state: undefined | Date) => void;
+}) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "w-[280px] justify-start text-left font-normal",
+            !value && "text-muted-foreground",
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {value ? format(value, "PPP") : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          selected={value}
+          onSelect={onValueChange}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default DatePicker;
